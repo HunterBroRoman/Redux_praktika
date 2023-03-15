1a9895209c21ff9723af6c424c73835aa7017916
Roman <sayga76239@gmail.com>
2023-03-15 11:08:38 +0100

    паака

--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { createStore } from 'redux';
 
-// const initialState = 0;
+const initialState = {value: 0};
 
-const reducer = (state = 0, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INC':
-      return state + 1;
+      return {
+        ...state,
+        value: state.value + 1,
+      }
     case 'DEC':
-      return state - 1;
+      return {
+        ...state,
+        value: state.value - 1,
+      }
+    case 'RND':
+      return {
+        ...state,
+        value: state.value * action.payload,
+      }
     default:
       return state;
   }
@@ -20,7 +31,7 @@ const store = createStore(reducer);
 
 const update = () => {
   document.getElementById('counter')
-  .textContent = store.getState();
+  .textContent = store.getState().value;
 }
 
 store.subscribe(update);
@@ -31,6 +42,11 @@ document.getElementById('inc').addEventListener('click',() => {
 
 document.getElementById('dec').addEventListener('click',() => {
   store.dispatch({ type: 'DEC' });
+})
+
+document.getElementById('rnd').addEventListener('click',() => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch({ type: 'RND', payload: value });
 })
 
 
